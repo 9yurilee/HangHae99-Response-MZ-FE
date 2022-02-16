41b17ad22380d9e6f44cb8aa758c0d18c99c140f
choi-sus <choi-sus@naver.com>
2022-02-17 05:24:38 +0900

    댓글 구현 끝

--- src/component/CommentList.js
+++ src/component/CommentList.js
@@ -1,60 +1,73 @@
 import React from "react";
-import {Grid, Text} from "../element/index";
-
+import {Grid, Text, Button} from "../element/index";
 import {useDispatch, useSelector} from "react-redux";
-// import {actionCreators as commentActions} from "../redux/modules/comment";
+import {actionCreators as commentActions} from "../redux/modules/comment";
 
 const CommentList = (props) => {
-  // const dispatch = useDispatch();
-  // const comment_list = useSelector(state => state.comment.list);
+  const dispatch = useDispatch();
+  const comment_list = useSelector(state => state.conmment.list);
+  const user_id = useSelector(state => state.user.user.user_id);
 
-  // const {post_id} = props;
-
-  // React.useEffect(() => {
-
-  //   if(!comment_list[post_id]){
-  //     dispatch(commentActions.getCommentFB(post_id));
-  //   }
-  // }, []);
+  const {article_id} = props;
   
-  // if(!comment_list[post_id] || !post_id){
-  //   return null;
-  // }
+  React.useEffect(() => {
+    if(!comment_list[article_id]){
+      dispatch(commentActions.getCommentDB(article_id));
+    }
+  }, []);
+  
+  if(!comment_list[article_id] || !article_id){
+    return null;
+  }
 
   return (
     <React.Fragment>
       <Grid padding="15px" border="2px solid #1f969c">
-        <CommentItem></CommentItem>
+        {
+          comment_list[article_id].map(c => {
+            return <CommentItem key={c.id} {...c} is_me={c.user_id === user_id}></CommentItem>
+          })
+        }
       </Grid>
     </React.Fragment>
   );
 };
 
 CommentList.defaultProps = {
-  post_id: null,
+  article_id: null,
+  is_me: false,
 }
 
 export default CommentList;
 
 
 const CommentItem = (props) => {
-    const {user_id, post_id, contents, insert_dt} = props;
+    const dispatch = useDispatch();
+    const {content, date, comment_id, article_id} = props;
+
+    const deleteComment = () => {
+      dispatch(commentActions.deleteCommentDB(article_id, comment_id))
+      window.alert("댓글이 삭제됐습니다!");
+      window.location.replace("/")
+    }
+
     return (
         <Grid is_flex_center>
             <Grid width="auto">
                 <Text letter bold size="18px">익명</Text>
             </Grid>
             <Grid is_flex_center>
-                <Text margin="0 0 0 25px" letter size="17px" minWidth="380px">{contents}</Text>
-                <Text margin="0 0 0 30px" size="12px">{insert_dt}</Text>
+                <Text margin="0 0 0 25px" letter size="17px" minWidth="380px">{content}</Text>
+                <Text margin="0 0 0 30px" size="12px">{date}</Text>
             </Grid>
+            {
+              props.is_me ? <Button width="100px" _onclick={deleteComment}>삭제</Button> : null
+            }
         </Grid>
     )
 }
 
 CommentItem.defaultProps = {
-    user_id: "",
-    post_id: 1,
-    contents: "귀여운 고양이!",
-    insert_dt: '2022-02-16 03:32:18'
+    content: "귀여운 고양이!",
+    date: '2022-02-16 03:32'
 }
--- src/redux/modules/comment.js
+++ src/redux/modules/comment.js
@@ -1,17 +1,16 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
-// import "moment";
-// import moment from "moment";
+import { api } from "../../shared/api";
 
 
 const SET_COMMENT = "SET_COMMENT";
 const ADD_COMMENT = "ADD_COMMENT";
-
+const DELETE_COMMENT = "DELETE_COMMENT"
 const LOADING = "LOADING";
 
-const setComment = createAction(SET_COMMENT, (post_id, comment_list) => ({post_id, comment_list}));
-const addComment = createAction(ADD_COMMENT, (post_id, comment) => ({post_id, comment}));
-
+const setComment = createAction(SET_COMMENT, (article_id, comment_list) => ({article_id, comment_list}));
+const addComment = createAction(ADD_COMMENT, (article_id, content) => ({article_id, content}));
+const deleteComment = createAction(DELETE_COMMENT, (article_id, comment_id) => ({article_id, comment_id}));
 const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 
 const initialState = {
@@ -19,20 +18,58 @@ const initialState = {
   is_loading: false,
 };
 
-const getCommentFB = (post_id) => {
-    return function(dispatch, getState, {history}){
+const getCommentDB = (article_id = null) => {
+    return function (dispatch, getState, { history }) {
+      api
+        .get(`/articles/${article_id}/comments`)
+        .then((res) => {
+          dispatch(setComment(article_id, res.data.comments))
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  }
 
+const addCommentDB = (article_id, content) => {
+    return function (dispatch, getState, { history }) {
+      api
+        .post(`/articles/${article_id}/comments`, {
+            content: content,
+        })
+        .then((res) => {
+            dispatch(addComment(article_id, {content: content}))
+        })
+        .catch((err) => {
+            console.error(err)
+        })
     }
 }
 
+const deleteCommentDB = (article_id, comment_id) => {
+    return function (dispatch, getState, { history }) {
+      api
+      .delete(`articles/${article_id}/comments/${comment_id}`)
+        .then((res) => {
+          dispatch(deleteComment(article_id, comment_id))
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  }
 
 export default handleActions(
   {
       [SET_COMMENT]: (state, action) => produce(state, (draft) => {
-
+        draft.list[action.payload.article_id] = action.payload.comment_list
       }),
       [ADD_COMMENT]: (state, action) => produce(state, (draft)=> {
-
+        draft.list[action.payload.article_id].push(action.payload.content)
+      }),
+      [DELETE_COMMENT]: (state, action) => produce(state, (draft) => {
+        draft.list[action.payload.article_id].filter((c, i) => c.comment_id !== action.payload.comment_id)
       }),
       [LOADING]: (state, action) => 
       produce(state, (draft) => {
@@ -43,9 +80,10 @@ export default handleActions(
 );
 
 const actionCreators = {
-  getCommentFB,
+  getCommentDB, addCommentDB, deleteCommentDB,
   setComment,
   addComment,
+  deleteComment
 };
 
 export { actionCreators };
