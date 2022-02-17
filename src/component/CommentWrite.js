66134ae1a69728ba6d117399f24b2b2cfed5400e
choi-sus <choi-sus@naver.com>
2022-02-17 17:32:42 +0900

    최종_최종_최종수정

--- src/component/CommentWrite.js
+++ src/component/CommentWrite.js
@@ -28,6 +28,7 @@ const CommentWrite = (props) => {
 
     dispatch(commentActions.addCommentDB(article_id, content))
     setContent("")
+    window.alert("댓글 작성이 완료되었습니다!")
     window.location.replace("/")
   }
 
