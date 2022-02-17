0de01788618b7a2ab7a7cad825f1b2ac7edf74db
degurrrrrr <degurrrrrr@gmail.com>
2022-02-17 19:03:50 +0900

    css 배경 적용 및 주석 정리

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -19,19 +19,13 @@ import { api } from "../shared/api";
 const Detail = (props) => {
   const dispatch = useDispatch();
   const post_list = useSelector((state) => state.post.list);
-  const user_info = useSelector((state) => state.user.user);
-  console.log(user_info);
+  const user_info = useSelector((state) => state.user.user)
   const id = props.match.params.post_id;
   const post_idx = post_list.findIndex((p) => p.post_id === id);
-  console.log(post_idx);
   const post_data = post_list[post_idx];
-  console.log(post_data);
 
   const [post, setPost] = React.useState(post_data ? post_data : null)
-
-  const editPost = () => {
-    dispatch(postActions.editPostFB())
-  }
+  
   const onDelete = async function() {
     if(window.confirm("정말로 삭제하시겠습니까?")){
       dispatch(postActions.deletePostFB(post_data.post_id))
@@ -45,17 +39,27 @@ const Detail = (props) => {
            <Text bold size="24px">
             상세페이지
           </Text>
-          <Post {...post} is_me={post_data.user_id === post_data?.id} />
+          <Post {...post} is_me={post.user_info.user_id === user_info?.uid} />
+          // {post_data.user_id === post_data?.id} 
 
           <Grid is_flex_center>
           </Grid>
           <Button
-            text="삭제"
+            text="수정"
             width="auto"
             padding="4px"
             margin="4px"
-            _onclick={onDelete}
-          />
+            _onclick={() => {
+               history.push(`/postwrite/${post_data.post_id}`);
+            }}
+            />
+            <Button
+              text="삭제"
+              width="auto"
+              padding="4px"
+              margin="4px"
+              _onclick={onDelete}
+            />
           <Comment>
             <CommentBox>
               <CommentList article_id={id}></CommentList>
